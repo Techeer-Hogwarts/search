387cd36b83bd18d9d45dc43d5cf81f36734d2363
Ryan <leehahnwoong@gmail.com>
2025-02-16 01:17:05 -0800

    feat: change output data of search

--- cmd/models/search_model.go
+++ cmd/models/search_model.go
@@ -22,7 +22,7 @@ type SearchResult struct {
 	Score float64 `json:"score" form:"score" example:"0.99"`
 }
 
-type IndexSearchResult map[string][]SearchResult
+type IndexSearchResult map[string]interface{}
 
 type FinalSearchResult struct {
 	Result IndexSearchResult `json:"result" form:"result"`
@@ -37,11 +37,82 @@ type CombinedSearchResult struct {
 }
 
 type UserSearchResult struct {
-	ID           string  `json:"id" form:"id" example:"1"`
-	Name         string  `json:"name" form:"name" example:"윤정은"`
-	Email        string  `json:"email" form:"email" example:"test@gmail.com"`
-	Year         int     `json:"year" form:"year" example:"7"`
-	ProfileImage string  `json:"profileImage" form:"profileImage" example:"https://example.com/profile.jpg"`
-	Index        string  `json:"index" form:"index" example:"user"`
-	Score        float64 `json:"score" form:"score" example:"0.99"`
+	ID           string   `json:"id" form:"id" example:"1"`
+	Name         string   `json:"name" form:"name" example:"윤정은"`
+	School       string   `json:"school" form:"school" example:"서울대학교"`
+	Email        string   `json:"email" form:"email" example:"test@gmail.com"`
+	Year         int      `json:"year" form:"year" example:"7"`
+	Grade        string   `json:"grade" form:"grade" example:"3"`
+	Stack        []string `json:"stack" form:"stack" example:"Go"`
+	ProfileImage string   `json:"profileImage" form:"profileImage" example:"https://example.com/profile.jpg"`
+	Index        string   `json:"index" form:"index" example:"user"`
+	Score        float64  `json:"score" form:"score" example:"0.99"`
+}
+
+type ProjectSearchResult struct {
+	ID             string   `json:"id" form:"id" example:"1"`
+	Name           string   `json:"name" form:"name" example:"프로젝트 이름"`
+	Title          string   `json:"title" form:"title" example:"프로젝트 제목"`
+	ProjectExplain string   `json:"projectExplain" form:"projectExplain" example:"프로젝트 설명"`
+	ResultImages   []string `json:"resultImages" form:"resultImages" example:"https://example.com/result.jpg"`
+	TeamStacks     []string `json:"teamStacks" form:"teamStacks" example:"Go"`
+	Index          string   `json:"index" form:"index" example:"user"`
+	Score          float64  `json:"score" form:"score" example:"0.99"`
+}
+
+type StudySearchResult struct {
+	ID             string   `json:"id" form:"id" example:"1"`
+	Name           string   `json:"name" form:"name" example:"프로젝트 이름"`
+	Title          string   `json:"title" form:"title" example:"프로젝트 제목"`
+	ProjectExplain string   `json:"projectExplain" form:"projectExplain" example:"프로젝트 설명"`
+	ResultImages   []string `json:"resultImages" form:"resultImages" example:"https://example.com/result.jpg"`
+	TeamStacks     []string `json:"teamStacks" form:"teamStacks" example:"Go"`
+	Index          string   `json:"index" form:"index" example:"user"`
+	Score          float64  `json:"score" form:"score" example:"0.99"`
+}
+
+type BlogSearchResult struct {
+	ID               string  `json:"id" form:"id" example:"1"`
+	Title            string  `json:"title" form:"title" example:"블로그 제목"`
+	URL              string  `json:"url" form:"url" example:"https://example.com/blog"`
+	Date             string  `json:"date" form:"date" example:"2021-01-01"`
+	UserID           string  `json:"userID" form:"userID" example:"1"`
+	UserName         string  `json:"userName" form:"userName" example:"윤정은"`
+	UserProfileImage string  `json:"userProfileImage" form:"userProfileImage" example:"https://example.com/profile.jpg"`
+	Stack            string  `json:"stack" form:"stack" example:"Go"`
+	Index            string  `json:"index" form:"index" example:"user"`
+	Score            float64 `json:"score" form:"score" example:"0.99"`
+}
+
+type ResumeSearchResult struct {
+	ID               string  `json:"id" form:"id" example:"1"`
+	Title            string  `json:"title" form:"title" example:"이력서 제목"`
+	URL              string  `json:"url" form:"url" example:"https://example.com/resume"`
+	CreatedAt        string  `json:"createdAt" form:"createdAt" example:"2021-01-01"`
+	UserID           string  `json:"userID" form:"userID" example:"1"`
+	UserName         string  `json:"userName" form:"userName" example:"윤정은"`
+	UserProfileImage string  `json:"userProfileImage" form:"userProfileImage" example:"https://example.com/profile.jpg"`
+	Stack            string  `json:"stack" form:"stack" example:"Go"`
+	Index            string  `json:"index" form:"index" example:"user"`
+	Score            float64 `json:"score" form:"score" example:"0.99"`
+}
+
+type SessionSearchResult struct {
+	ID        string  `json:"id" form:"id" example:"1"`
+	Thumbnail string  `json:"thumbnail" form:"thumbnail" example:"https://example.com/thumbnail.jpg"`
+	Title     string  `json:"title" form:"title" example:"세션 제목"`
+	Presenter string  `json:"presenter" form:"presenter" example:"윤정은"`
+	Date      string  `json:"date" form:"date" example:"2021-01-01"`
+	LikeCount int     `json:"likeCount" form:"likeCount" example:"10"`
+	ViewCount int     `json:"viewCount" form:"viewCount" example:"100"`
+	Index     string  `json:"index" form:"index" example:"user"`
+	Score     float64 `json:"score" form:"score" example:"0.99"`
+}
+
+type EventSearchResult struct {
+	ID       string  `json:"id" form:"id" example:"1"`
+	Category string  `json:"category" form:"category" example:"세션"`
+	Title    string  `json:"title" form:"title" example:"세션 제목"`
+	Index    string  `json:"index" form:"index" example:"user"`
+	Score    float64 `json:"score" form:"score" example:"0.99"`
 }
--- cmd/repositories/repository.go
+++ cmd/repositories/repository.go
@@ -6,5 +6,11 @@ type Repository interface {
 	Search(index, query string, limit, offset int) ([]models.CombinedSearchResult, error)
 	TitleSearch(index, query string, limit, offset int) ([]models.CombinedSearchResult, error)
 	UserSearch(query string, limit, offset int) ([]models.UserSearchResult, error)
-	FinalSearch(index, query string, limit, offset int) ([]models.SearchResult, error)
+	ProjectSearch(query string, limit, offset int) ([]models.ProjectSearchResult, error)
+	StudySearch(query string, limit, offset int) ([]models.StudySearchResult, error)
+	BlogSearch(query string, limit, offset int) ([]models.BlogSearchResult, error)
+	ResumeSearch(query string, limit, offset int) ([]models.ResumeSearchResult, error)
+	SessionSearch(query string, limit, offset int) ([]models.SessionSearchResult, error)
+	EventSearch(query string, limit, offset int) ([]models.EventSearchResult, error)
+	// FinalSearch(index, query string, limit, offset int) ([]models.SearchResult, error)
 }
