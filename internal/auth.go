02b70e570f5deb59adb5b5fca9a4677613ca1a10
Ryan <leehahnwoong@gmail.com>
2025-04-05 00:47:30 -0700

    fix: logs

--- internal/auth.go
+++ internal/auth.go
@@ -2,6 +2,7 @@ package internal
 
 import (
 	"errors"
+	"fmt"
 	"log"
 
 	"github.com/Techeer-Hogwarts/search/config"
@@ -19,6 +20,7 @@ var JWT_TOKEN string
 
 func init() {
 	JWT_TOKEN = config.GetEnvVarAsString("JWT_SECRET", "some_secret_key")
+	log.Printf("Loaded JWT secret: %s", JWT_TOKEN)
 }
 
 // ValidateJWT middleware checks the JWT token from cookies
@@ -26,6 +28,7 @@ func ValidateJWT() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		// Get Cookie value
 		cookie, err := c.Cookie("access_token")
+		log.Println("Token from cookie:", cookie)
 		if err != nil {
 			// Invalid JWT, allow the request to continue
 			c.Set("valid_jwt", false) // Flag indicating invalid JWT
@@ -52,10 +55,16 @@ func ValidateJWT() gin.HandlerFunc {
 
 // validateToken verifies the JWT and extracts claims
 func validateToken(tokenString string) (*JWTClaims, error) {
+	// token, err := jwt.ParseWithClaims(tokenString, &JWTClaims{}, func(token *jwt.Token) (interface{}, error) {
+	// 	// Ensure signing method is HMAC
+	// 	if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+	// 		return nil, errors.New("unexpected signing method")
+	// 	}
+	// 	return []byte(JWT_TOKEN), nil
+	// })
 	token, err := jwt.ParseWithClaims(tokenString, &JWTClaims{}, func(token *jwt.Token) (interface{}, error) {
-		// Ensure signing method is HMAC
-		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-			return nil, errors.New("unexpected signing method")
+		if token.Method.Alg() != jwt.SigningMethodHS256.Alg() {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
 		}
 		return []byte(JWT_TOKEN), nil
 	})
